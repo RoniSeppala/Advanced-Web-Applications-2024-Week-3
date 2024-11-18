d67bb6c3659bcb0ddd268563b1d36a7d47784251
Roni Seppälä <roni.o.seppala@gmail.com>
2024-11-18 16:00:29 +0200

    added third feature

--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,24 @@ router.get("/hello", (req: Request, res: Response) => {
 router.get("/echo/:id", (req: Request, res: Response) => {
     let id: string = req.params.id
     try {res.json({"id": id})
-} catch (error: any) {
-    console.log(`Error parsing JSON: ${error}`)
-}
+    } catch (error: any) {
+        console.log(`Error parsing JSON: ${error}`)
+    }
 })
 
+router.post("/sum", (req: Request, res: Response) => {
+    let numbers: number[] = req.body.numbers
+    let sum: number = 0
+
+    for (let index = 0; index < numbers.length; index++) {
+        const element = numbers[index];
+        sum = sum + element
+
+    }
+    try{
+        res.json({"sum": sum})
+    } catch (error: any) {
+        console.log(`Error parsin JSON: ${error}`)
+    }
+})
 export default router
