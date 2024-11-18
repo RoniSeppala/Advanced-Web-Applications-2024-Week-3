88b4d4f04cdfbf313145fa6238b54c9776c89b64
Roni Seppälä <roni.o.seppala@gmail.com>
2024-11-18 20:49:25 +0200

    fix 4b

--- dist/src/index.js
+++ dist/src/index.js
@@ -38,7 +38,7 @@ router.post("/users", (req, res) => {
     const { name, email } = req.body;
     const newUser = { name, email };
     users.push(newUser);
-    res.json("User successfully added");
+    res.json({ "message": "User successfully added" });
 });
 router.get("/users", (req, res) => {
     console.log(users);
--- src/index.ts
+++ src/index.ts
@@ -48,7 +48,7 @@ router.post("/users", (req: Request, res: Response) => {
     const {name, email} = req.body
     const newUser: TUser = {name, email};
     users.push(newUser)
-    res.json("User successfully added")
+    res.json({"message":"User successfully added"})
 })
 
 router.get("/users", (req: Request, res: Response) => {
