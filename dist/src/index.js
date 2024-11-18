0201384165339bd1524e5865ea2147ed31843ac5
Roni Seppälä <roni.o.seppala@gmail.com>
2024-11-18 15:35:04 +0200

    added second feature

--- dist/src/index.js
+++ dist/src/index.js
@@ -5,4 +5,8 @@ const router = (0, express_1.Router)();
 router.get("/hello", (req, res) => {
     res.json({ "msg": "Hello World!" });
 });
+router.get("/echo/:id", (req, res) => {
+    let id = req.params.id;
+    res.json({ "id": id });
+});
 exports.default = router;
--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,9 @@ router.get("/hello", (req: Request, res: Response) => {
     res.json({"msg":"Hello World!"})
 })
 
+router.get("/echo/:id", (req: Request, res: Response) => {
+    let id: string = req.params.id
+    res.json({"id": id})
+})
+
 export default router
